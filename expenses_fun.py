b1bd1fe64b056c43ad20466f2800ca70841c773d
student <sonamarkova849@gmail.com>
2024-04-08 15:48:31 +0900

    русские кнопки

--- expenses_fun.py
+++ expenses_fun.py
@@ -22,11 +22,11 @@ async def staistics_expenses(update, context):
     )
 
 
-reply_keyboard_expenses = [['/staistics_expenses', '/show_expenses']]
+reply_keyboard_expenses = [['Статистика расходов', 'Посмотреть расходы']]
 markup_timer_expenses = ReplyKeyboardMarkup(reply_keyboard_expenses, one_time_keyboard=False)
 
-reply_keyboard_staistics_expenses = [['/магазины', '/образование']]
+reply_keyboard_staistics_expenses = [['магазины', '/образование']]
 markup_staistics_expenses = ReplyKeyboardMarkup(reply_keyboard_staistics_expenses, one_time_keyboard=False)
 
-reply_keyboard_show_expenses = [['/1 неделя', '/2 недели']]
+reply_keyboard_show_expenses = [['1 неделя', '2 недели']]
 markup_show_expenses = ReplyKeyboardMarkup(reply_keyboard_show_expenses, one_time_keyboard=False)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from data import db_session
 from incomes_fun import income, income_period, add_income
 from start_fun import start
@@ -10,18 +10,26 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+income_had = MessageHandler(filters.Text(['Доходы']), income)
+expense_had = MessageHandler(filters.Text(['Расходы']), expenses)
+add_income_had = MessageHandler(filters.Text(['Добавить доход']), add_income)
+income_period_had = MessageHandler(filters.Text(['Доходы за определенный период']), income_period)
+show_expenses_had = MessageHandler(filters.Text(['Посмотреть расходы']), show_expenses)
+staistics_expenses_had = MessageHandler(filters.Text(['Статистика расходов']), staistics_expenses)
 
 
 def main():
     application = Application.builder().token('7031701033:AAFvF1ARw2Ag9A0qdIGYb17MHLsD843fQ0U').build()
-
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("income", income))
-    application.add_handler(CommandHandler("expenses", expenses))
-    application.add_handler(CommandHandler("income", income))
-    application.add_handler(CommandHandler("income_period", income_period))
-    application.add_handler(CommandHandler("add_income", add_income))
-    application.add_handler(CommandHandler("show_expenses", show_expenses))
+    application.add_handler(add_income_had)
+    application.add_handler(show_expenses_had)
+    application.add_handler(income_period_had)
+    application.add_handler(income_had)
+    application.add_handler(expense_had)
+    application.add_handler(staistics_expenses_had)
+
+
+
     application.add_handler(CommandHandler("staistics_expenses", staistics_expenses))
     db_session.global_init("db/money.db")
     application.run_polling()
