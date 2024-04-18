f26c0e3186d0ce33987a9ed32ea8c2990f738cc5
dzumka <151499495+juliadddddddddddddddddddddddddd@users.noreply.github.com>
2024-04-18 12:27:36 +0300

    Update incomes_fun.py

--- incomes_fun.py
+++ incomes_fun.py
@@ -1,14 +1,10 @@
+from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import ConversationHandler
 from data import db_session
 from data.users import User
 from data.incomes import Income
 import datetime
 from start_fun import markup
-import datetime
-
-from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters
-from data.incomes import Income
 
 reply_keyboard_income = [['Добавить доход', 'Доходы за определенный период']]
 markup_income = ReplyKeyboardMarkup(reply_keyboard_income, one_time_keyboard=True)
@@ -56,36 +52,33 @@ async def income_period(update, context):
         reply_markup=markup_income_period
     )
 
-def incomess_period_db(update, context):
+def show_income_bd(update, context):
     db_sess = db_session.create_session()
     user_ = update.effective_user
     cur_user = db_sess.query(User).filter(User.name.like(user_.id)).first()
-    cur_period = db_sess.query(Income).filter(Income.date.like(update.message.text)).first()
-    moneys = db_sess.query(Income.money, Income.date).filter(Income.date == cur_period).filter(
+    if update.message.text == '1 неделя':
+        need_date = datetime.datetime.now() - datetime.timedelta(days=7)
+    elif update.message.text == '2 недели':
+        need_date = datetime.datetime.now() - datetime.timedelta(days=14)
+    elif update.message.text == 'Месяц':
+        need_date = datetime.datetime.now() - datetime.timedelta(days=30)
+    else:
+        return 'ошибка'
+
+    moneys = db_sess.query(Income.money, Income.date).filter(
+        Income.date >= need_date).filter(
         Income.user == cur_user).all()
     res = []
-
     for i in moneys:
         res.append(f'{i[0]} - дата {str(i[1])[:10]}')
     moneys = '\n'.join(res)
     return moneys
 
 
-async def add_income_answer(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
-    context.user_data['income'] = int(update.message.text)
+async def show_income_answer(update, context):
+    result = show_income_bd(update, context)
     await update.message.reply_text(
-        f"Доход внесен в базу данных")
-    add_income_bd(update, context)
-
+        result,
+        reply_markup=markup
+    )
     return ConversationHandler.END
-
-
-def add_income_bd(update, context):
-    db_sess = db_session.create_session()
-    cur_user = db_sess.query(User).filter(User.id == context.user_data['user_id']).first()
-    income = Income(money=context.user_data['income'], data=datetime.datetime.now(), user=cur_user)
-    db_sess.add(income)
-    db_sess.commit()
-    db_sess.close()
