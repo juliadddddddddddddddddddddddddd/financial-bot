1036a98350f8457b8bea51e1ea4162f090edc26f
dzumka <151499495+juliadddddddddddddddddddddddddd@users.noreply.github.com>
2024-04-22 19:02:06 +0300

    Update files_fun.py
    Update files_fun.py

--- files_fun.py
+++ files_fun.py
@@ -4,22 +4,35 @@ import datetime
 
 res = []
 
-async def files_fun_m(update, context):
+async def get_photo(update, context):
     await update.message.reply_text(
-        'Пришлите фотографию')
-    file = await context.bot.get_file(update.message.photo)
-    file_name = fr"C:\Users\Юлия\PycharmProjects\pythonProject4\sources\{file}"
-    await file.download_to_drive(file_name)
-    res.append(file_name)
+        'Загрузите фото')
     return 1
-    
-def file_get():
-    file = open(fr'C:\Users\Юлия\PycharmProjects\pythonProject4\sources\{random.choice(res)}.jpeg', 'rb')
-    return file
-    
-async def files_fun_m_answer(update, context):
-    if len(res) != 0:
-        await update.message.reply_photo(file_get(), has_spoiler=True)
-    else:
-        update.message.reply_text("Извините, но загруженных фотографий нет")
-    return ConversationHandler.END
+
+async def upload_photo(update, context):
+    file_id = update.message.document.file_id
+    new_file = await context.bot.get_file(file_id)
+    await new_file.download_to_drive()
+    res.append(new_file)
+    print(res)
+
+    # НЕ РАБОТАЕТ!
+    # new_file = await update.message.effective_attachment.get_file()
+    # file_name = fr"C:\Users\Юлия\PycharmProjects\pythonProject4\sources\{new_file}"
+    # await new_file.download_to_drive(file_name)
+
+
+    # НЕ РАБОТАЕТ!
+    # new_file = await update.message.effective_attachment[-1].get_file()
+    # file_name = fr"C:\Users\Юлия\PycharmProjects\pythonProject4\sources\{new_file}"
+    # await new_file.download_to_drive(file_name)
+    # res.append(file_name)
+    # print(res)
+
+    # НЕ РАБОТАЕТ!
+    # file = await context.bot.get_file(update.message.photo)
+    # file_name = fr"C:\Users\Юлия\PycharmProjects\pythonProject4\sources\{file}"
+    # await file.download_to_drive(custom_path=file_name)
+    # res.append(file_name)
+
+
